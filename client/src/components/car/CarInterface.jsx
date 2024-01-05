71dc7ec900be6a2433395262d25a0af56dfd2c64
JasontheOmnivorous <marsjason485@gmail.com>
2024-01-05 22:43:14 +0630

    buttons

--- client/src/components/car/CarInterface.jsx
+++ client/src/components/car/CarInterface.jsx
@@ -6,11 +6,19 @@ const CarInterface = ({ direction }) => {
     <div className="flex flex-col justify-center items-center">
       <h1 className="m-4 text-xl font-bold">{direction}</h1>
       <div className="border border-red-600 p-2 rounded-xl shadow-lg flex flex-col items-start justify-center gap-2">
-        <img className="w-16 h-16" src={CarSeat} alt="car-seat" />
+        <button>
+          <img className="w-16 h-16" src={CarSeat} alt="seat-1" />
+        </button>
         <div className="flex justify-start items-center">
-          <img className="w-16 h-16" src={CarSeat} alt="car-seat" />
-          <img className="w-16 h-16" src={CarSeat} alt="car-seat" />
-          <img className="w-16 h-16" src={CarSeat} alt="car-seat" />
+          <button>
+            <img className="w-16 h-16" src={CarSeat} alt="seat-2" />
+          </button>
+          <button>
+            <img className="w-16 h-16" src={CarSeat} alt="seat-3" />
+          </button>
+          <button>
+            <img className="w-16 h-16" src={CarSeat} alt="seat-4" />
+          </button>
         </div>
       </div>
     </div>
