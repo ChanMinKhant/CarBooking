c75024500b231c9404d11aaa34031b669fff16c9
Chan Min Khant <jiyonfire@gmail.com>
2024-02-10 17:21:46 +0630

    Add domain to cookie configuration

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -78,6 +78,7 @@ exports.login = asyncErrorHandler(async (req, res, next) => {
     httpOnly: true,
     secure: true,
     path: '/',
+    domain: process.env.CLIENT_URL,
   });
 
   res.status(200).json({
