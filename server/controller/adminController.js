07a296d8daaf05dc7135b0b0233623cf1aeca87a
Chan Min Khant <jiyonfire@gmail.com>
2024-02-10 17:17:25 +0630

    Update secure flag and add path to cookie

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -76,8 +76,10 @@ exports.login = asyncErrorHandler(async (req, res, next) => {
       Date.now() + (process.env.COOKIE_EXPIRES_IN || 20) * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: process.env.NODE_ENV.trim() === 'production' ? true : false,
+    secure: true,
+    path: '/',
   });
+
   res.status(200).json({
     success: true,
     token,
